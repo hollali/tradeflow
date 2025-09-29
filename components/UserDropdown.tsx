6961c68cd438871a1102e8ea0b84e9b4dc684799
hollali <dheztinykartel@gmail.com>
2025-09-29 12:43:53 +0000

    `feat: updated UserDropdown component to use @radix-ui/react-avatar`

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,6 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
 
 const UserDropdown = () => {
   const router = useRouter();
@@ -17,9 +19,23 @@ const UserDropdown = () => {
     router.push("/sign-in");
   };
 
+  const user = { name: "John Doe", email: "John@gmail.com" }; // Example user object
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-3 text-gray-4 hover:bg-yellow-500"
+        >
+          <Avatar className="w-8 h-8">
+            {/*<AvatarImage src="https://github.com/shadcn.png" />*/}
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              {user.name.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
