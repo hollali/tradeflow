9e09b1ae6bb18947fa19644375f71e6bbea4393e
hollali <dheztinykartel@gmail.com>
2025-09-29 13:09:29 +0000

    `Refactor UserDropdown component: add user information and avatar`

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -29,7 +29,7 @@ const UserDropdown = () => {
           className="flex items-center gap-3 text-gray-4 hover:bg-yellow-500"
         >
           <Avatar className="w-8 h-8">
-            {/*<AvatarImage src="https://github.com/shadcn.png" />*/}
+            <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
               {user.name.charAt(0)}
             </AvatarFallback>
@@ -42,14 +42,21 @@ const UserDropdown = () => {
           </div>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleSignOut}>Sign Out</DropdownMenuItem>
+      <DropdownMenuContent className="text-gray-400">
+        <div className="flex relative items-center gap-3 py-2">
+          <Avatar className="w-10 h-10">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              {user.name.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+          <div className="hidden md:flex flex-col items-start">
+            <span className="text-base font-medium text-gray-400">
+              {user.name}
+            </span>
+            <span className="text-sm text-gray-500">{user.email}</span>
+          </div>
+        </div>
       </DropdownMenuContent>
     </DropdownMenu>
   );
