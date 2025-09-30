b30ae1b1d1f0a65bc7c3558b3ff3dacdf2e44414
hollali <dheztinykartel@gmail.com>
2025-09-30 02:39:00 +0000

    `feat: updated auth layout component with new design and features`

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,59 @@
-const layout = () => {
-  return <div>layout</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="auth-layout">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt=""
+            width={140}
+            height={32}
+            className="h-8 w-auto"
+          />
+        </Link>
+        <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+      </section>
+      <section className="auth-right-section">
+        <div className="z-10 relative lg:mt-4 lg:mb-16">
+          <blockquote className="auth-blockquote">
+            "Empowering Traders, Enriching Lives"
+          </blockquote>
+          <div className="mt-6 flex items-center justify-between">
+            <div>
+              <cite className="auth-testimonial-author">Hollali Kelvin</cite>
+              <p className="max-md:text-xs text-gray-500">
+                CEO, TradeFlow Inc.
+              </p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Image
+                  key={star}
+                  src="/assets/icons/star.svg"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  className="w-5 h-5"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 relative">
+          <Image
+            src="/assets/images/dashboard.png"
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default layout;
