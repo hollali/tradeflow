88bebfc50ad40c708b74742bf9d035523108f08e
hollali <dheztinykartel@gmail.com>
2025-09-30 10:03:03 +0000

    `feat: updated app/auth/layout.tsx with new imports and layout changes`

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,39 +1,47 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+//import {auth} from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  //const session = await auth.api.getSession({ headers: await headers() })
+
+  //if(session?.user) redirect('/')
+
   return (
     <main className="auth-layout">
       <section className="auth-left-section scrollbar-hide-default">
         <Link href="/" className="auth-logo">
           <Image
             src="/assets/icons/logo.svg"
-            alt=""
+            alt="Signalist logo"
             width={140}
             height={32}
             className="h-8 w-auto"
           />
         </Link>
+
         <div className="pb-6 lg:pb-8 flex-1">{children}</div>
       </section>
+
       <section className="auth-right-section">
         <div className="z-10 relative lg:mt-4 lg:mb-16">
           <blockquote className="auth-blockquote">
-            "Empowering Traders, Enriching Lives"
+            Signalist turned my watchlist into a winning list. The alerts are
+            spot-on, and I feel more confident making moves in the market
           </blockquote>
-          <div className="mt-6 flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <div>
-              <cite className="auth-testimonial-author">Hollali Kelvin</cite>
-              <p className="max-md:text-xs text-gray-500">
-                CEO, TradeFlow Inc.
-              </p>
+              <cite className="auth-testimonial-author">- Ethan R.</cite>
+              <p className="max-md:text-xs text-gray-500">Retail Investor</p>
             </div>
             <div className="flex items-center gap-0.5">
               {[1, 2, 3, 4, 5].map((star) => (
                 <Image
-                  key={star}
                   src="/assets/icons/star.svg"
-                  alt="star"
+                  alt="Star"
+                  key={star}
                   width={20}
                   height={20}
                   className="w-5 h-5"
@@ -42,6 +50,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
             </div>
           </div>
         </div>
+
         <div className="flex-1 relative">
           <Image
             src="/assets/images/dashboard.png"
@@ -55,5 +64,4 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     </main>
   );
 };
-
-export default layout;
+export default Layout;
